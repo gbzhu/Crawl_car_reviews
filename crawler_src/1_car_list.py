d36f705a190546e4586b43d0289a2589c97cd311
gbzhu <gbzhu@zju.edu.cn>
2018-07-30 16:56:36 +0800

    instruct a function to the config file

--- crawler_src/1_car_list.py
+++ crawler_src/1_car_list.py
@@ -3,27 +3,31 @@ import json
 import configparser
 from crawler_src.db_manager import DBManger
 
-makes = ['Acura', 'Alfa Romeo', 'Aston Martin', 'Audi', 'BMW', 'Bentley', 'Bugatti', 'Buick', 'Cadillac', 'Chevrolet',
-         'Chrysler', 'Dodge', 'Ferrari', 'Fiat', 'Ford', 'GMC', 'Genesis', 'Honda', 'Hyundai', 'Infiniti', 'Jaguar',
-         'Jeep', 'Kia', 'Koenigsegg', 'Lamborghini', 'Land Rover', 'Lexus', 'Lincoln', 'Lotus', 'Maserati', 'Mazda',
-         'McLaren', 'Mercedes-AMG', 'Mercedes-Benz', 'Mercedes-Maybach', 'Mini', 'Mitsubishi', 'Nissan', 'Pagani',
-         'Polestar', 'Porsche', 'Ram', 'Rolls-Royce', 'Scion', 'Smart', 'Spyker', 'Subaru', 'Tesla', 'Toyota',
-         'Volkswagen', 'Volvo']
 
-pre_url = 'https://www.caranddriver.com/list-reviews-in-depth'
+def config_parser(filepath: str):
+    cp = configparser.ConfigParser()
+    cp.read(filenames=filepath, encoding='utf8')
+    host = cp['mongodb']['host']
+    port = cp['mongodb']['port']
+    database = cp['mongodb']['database']
+    makes = cp['car_info']['makes']
+    pre_api = cp['car_info']['pre_api']
+    suf_api = cp['car_info']['suf_api']
+    pre_url = cp['car_info']['pre_url']
+    return host, int(port), database, makes, pre_api, suf_api, pre_url
 
 
-def joint_url(make: str):
+def joint_url(make: str, pre_api: str, suf_api: str):
     make = make.replace(' ', '-').lower()
-    return 'https://api-prod.caranddriver.com/v1/models/make/' + make + '?is_hidden=false&is_in_navigation=true'
+    return pre_api + make + suf_api
 
 
-def obtain_model_and_joint(db_manager: DBManger):
+def obtain_model_and_joint(db_manager: DBManger, makes: list, pre_api: str, suf_api: str, pre_url: str):
     for make in makes:
 
         print(make)
 
-        make_url = joint_url(make=make)
+        make_url = joint_url(make=make, pre_api=pre_api, suf_api=suf_api)
         model_json = {}
         with request.urlopen(make_url) as resp:
             context = resp.read().decode('utf-8')
@@ -47,10 +51,7 @@ def obtain_model_and_joint(db_manager: DBManger):
 
 
 if __name__ == '__main__':
-    cp = configparser.ConfigParser()
-    cp.read(filenames='../config.ini', encoding='utf8')
-    host = cp['mongodb']['host']
-    port = cp['mongodb']['port']
-    database = cp['mongodb']['database']
-    db_manager = DBManger(host=host, port=int(port), db=database)
-    obtain_model_and_joint(db_manager=db_manager)
+    host, port, database, makes, pre_api, suf_api, pre_url = config_parser('../config.ini')
+    makes = makes.aplit(',')
+    db_manager = DBManger(host=host, port=port, db=database)
+    obtain_model_and_joint(db_manager=db_manager, makes=makes, pre_api=pre_api, suf_api=suf_api, pre_url=pre_url)
---
+++ crawler_src/2_car_detail.py
@@ -0,0 +1,9 @@
+from crawler_src.db_manager import DBManger
+
+
+def get_review():
+    pass
+
+
+if __name__ == '__main__':
+    pass
