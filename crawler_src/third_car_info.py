8e718e0b8d797cd6caa11994ccd6529406f8f76d
gbzhu <gbzhu@zju.edu.cn>
2018-08-03 17:16:10 +0800

    deal the 404

--- crawler_src/third_car_info.py
+++ crawler_src/third_car_info.py
@@ -2,17 +2,17 @@ from crawler_src.db_manager import DBManger
 from crawler_src.fisrt_car_list import config_parser
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError
 
 
 def obtain_car_info(url: str, target_website: str):
     reviews = {}
-    finish_url = []
-    if 'page' in url:
-        url = url.split('-review-')[0] + '-review'
-    if url in finish_url:
-        return
-    with urlopen(url) as resp:
-        context = resp.read().decode('utf-8')
+    try:
+        with urlopen(url) as resp:
+            context = resp.read().decode('utf-8')
+    except HTTPError as e:
+        print(e.msg)
+        return None
     bs4_obj = BeautifulSoup(context, 'lxml')
     ng_parent = bs4_obj.find('div',
                              class_='w100p w180-dk w300-tb hauto border-top-dotted-tb border-gainsboro pb1 pt4-tb block mt4 mt2-dk mb5-dk hide-tb ph20 ph0-nm mr8-dk')
@@ -34,12 +34,14 @@ def obtain_car_info(url: str, target_website: str):
         if 'Overview' in ng:
             ng_info = obtain_ng_info(bs4_obj)
         else:
-            with urlopen(ng_url[ng]) as resp_2:
-                context_2 = resp_2.read().decode('utf-8')
+            try:
+                with urlopen(ng_url[ng]) as resp_2:
+                    context_2 = resp_2.read().decode('utf-8')
+            except:
+                continue
             bs4_obj_2 = BeautifulSoup(context_2, 'lxml')
             ng_info = obtain_ng_info(bs4_obj_2)
         reviews[ng] = ng_info
-    finish_url.append(url)
     return reviews
 
 
@@ -88,6 +90,7 @@ if __name__ == '__main__':
         make_model = car_detail['make_model']
 
         cars = car_detail['cars']
+        finish_url = []
         for car in cars:
             for key, value in car.items():
                 brand = key
@@ -95,7 +98,17 @@ if __name__ == '__main__':
                 print(make_model + " : " + brand)
 
                 href = value['href']
+                if 'page' in href:
+                    href = href.split('-review-')[0] + '-review'
+                    brand = href.split('/reviews/')[1].split('-in-depth-model-')[0]
+
+                    print(make_model + " : " + brand)
+
+                if href in finish_url:
+                    print("it's finished")
+                    continue
                 reviews = obtain_car_info(href, 'https://www.caranddriver.com')
+                finish_url.append(href)
                 if reviews is None:
                     continue
                 reviews['car_info'] = make_model + " / " + brand
